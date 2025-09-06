70caad3d6109c158ba0c89194e15388abb79c14a
CaspianRabbit <pushprenushekhawat@gmail.com>
2025-09-06 22:58:24 +0530

    Learned a bit about data types and variables

---
+++ 030925/variables0.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int variable = 18;
+    // integers are 4 bytes
+    // signed int approx range of -2b to 2b {using 2^32}
+    
+    cout << variable << endl;
+    
+    int v = variable;
+    cout << v << " to the power of 2 gives " << v * v << endl;
+}
